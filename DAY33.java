ad4013696d94d6b4e21cff7d9f257f114cb743ef
Ritesh  Gaikwad <95370359+Riteshgaikwad@users.noreply.github.com>
2023-02-25 00:58:00 +0530

    Create DAY33.java

---
+++ DAY33.java
@@ -0,0 +1,6 @@
+/*
+Write a Program to check if String is a palindrome or not
+Description
+Get an input string from the user and then check whether it is a palindrome string or not.
+*/
+//Solution:-
