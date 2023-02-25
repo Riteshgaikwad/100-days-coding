b1a6d8dd54f813ecef79ac487804e320cb44bea0
Ritesh  Gaikwad <95370359+Riteshgaikwad@users.noreply.github.com>
2023-02-25 19:14:19 +0530

    Update DAY33.java

--- DAY33.java
+++ DAY33.java
@@ -4,3 +4,30 @@ Description
 Get an input string from the user and then check whether it is a palindrome string or not.
 */
 //Solution:-
+import java.util.*;
+
+public class basic {
+    public static boolean is_palindrome(String str){
+        int start=0;
+        int end=str.length()-1;
+        while(start<end){
+            if(str.charAt(start)!=str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string:-");
+        String str=sc.nextLine();       //Taking string as an input
+        if(is_palindrome(str)){
+            System.out.println("The string is palindrome");
+        }
+        else{
+            System.out.println("The string is not palindrome");
+        }
+    }
+}
